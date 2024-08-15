0c6925311752d5cc1babee0da0214388ba999fed
dasdgdafg <dasdgdafg@cock.li>
2024-08-15 02:46:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-const urlParams = new URLSearchParams(queryString);
+const urlParams = new URLSearchParams(window.location.search);
 const soundURL = urlParams.get('sound');
 const streamer = urlParams.get('streamer');
-/*const exampleSocket = new WebSocket(
+
+const exampleSocket = new WebSocket(
   "wss://pomf.tv/websocket/"
 );
 
 exampleSocket.onopen = (event) => {
-  exampleSocket.send('{"roomId":"ktgd420","userId":"0","apikey":"Guest","action":"connect"}');
+  exampleSocket.send('{"roomId":"' + streamer + '","userId":"0","apikey":"Guest","action":"connect"}');
 };
 
 exampleSocket.onmessage = (event) => {
-  console.log(event.data);
   d = JSON.parse(event.data)
-  console.log(d.type);
-};*/
-setTimeout(async function() {
-  await newSub("abc");
-  await newSub("def");
-  await newSub("ghi");
-}, 2000)
+  if (d.type == 'new-subscriber') {
+    await newSub(d.subscriber.colourName)
+  }
+};
 
 var audio = new Audio(soundURL);
 
 async function newSub(name) {
-  document.getElementById("sub").textContent = name;
-  console.log("showing");
+  document.getElementById("sub").innerHTML = name;
+  //console.log("showing");
   document.getElementById("main").classList.add("show");
-  console.log("playing");
+  //console.log("playing");
   audio.play();
-  console.log("waiting");
+  //console.log("waiting");
   await new Promise(r => setTimeout(r, 10000));
-  console.log("hiding");
+  //console.log("hiding");
   document.getElementById("main").classList.remove("show");
-  console.log("waiting");
+  //console.log("waiting");
   await new Promise(r => setTimeout(r, 2000));
 }
