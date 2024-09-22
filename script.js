5b586bd6eb8d6d048fb4fe850ad7dd6dd4727e9b
SunshineLollipops6 <lollipopssunshine6@gmail.com>
2024-09-22 00:09:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -27,21 +27,15 @@ function reconnect() {
 var audio = new Audio(soundURL);
 
 async function newSub(name) {
+
   document.getElementById("sub").innerHTML = name;
-  //console.log("showing");
-  document.getElementById("main").classList.add("show");
   //console.log("playing");
   try {
     audio.play();
   } finally {
     // ...
   }
-  //console.log("waiting");
-  await new Promise(r => setTimeout(r, 10000));
-  //console.log("hiding");
-  document.getElementById("main").classList.remove("show");
-  //console.log("waiting");
-  await new Promise(r => setTimeout(r, 2000));
+
 }
 
 reconnect();
